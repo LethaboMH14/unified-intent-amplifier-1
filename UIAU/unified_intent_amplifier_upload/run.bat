d9d6cd9c2bc912b03c0fa251a27a22e5eb1c2a0d
LethaboMH14 <25055682@mylife.unisa.ac.za>
2026-03-30 02:36:22 +0200

    Delete UIAU directory

--- UIAU/unified_intent_amplifier_upload/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Clearing Python cache...
-cd /d "%~dp0"
-if exist __pycache__ rmdir /s /q __pycache__
-echo Cache cleared.
-echo.
-echo Starting Unified Intent Amplifier...
-python main.py
-pause
